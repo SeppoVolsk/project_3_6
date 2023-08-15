77422b0645d6f3625d097eea4c790b0451f3becf
serejkuz <141903834+i-wanderas@users.noreply.github.com>
2023-08-15 09:39:49 +0300

    add conftest add test_items

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_items(browser):
+    browser.get(link)
+    assert browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']"), \
+        "The button was not found!"
+
