5d0ec43aabe4bd71eac0f8bbb0836311019199bd
serejkuz <141903834+i-wanderas@users.noreply.github.com>
2023-08-15 09:47:54 +0300

    rename test

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_items(browser):
+def test_add_button_is_displayed(browser):
     browser.get(link)
     assert browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']"), \
         "The button was not found!"
