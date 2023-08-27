1562479885310c8263f59bfb91f5b500fc978eb0
SeppoVolsk <serjanya415@gmail.com>
2023-08-27 11:02:12 +0300

    add locator variable for test workflow

--- test_items.py
+++ test_items.py
@@ -3,10 +3,11 @@ import time
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+locator = "//button[@class='btn btn-lg btn-primary btn-add-to-basket']"
 
 
 def test_add_button_is_displayed(browser):
     browser.get(link)
-    assert browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']"), \
+    assert browser.find_element(By.XPATH, locator), \
         "The button was not found!"
 
